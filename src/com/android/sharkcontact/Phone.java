8242292c67aa07a6c7f1dc624f9ddb3e776a168b
unknown <mobile@.(none)>
2011-02-17 18:33:29 +0800

    实现了获取手机联系人的信息

--- src/com/android/sharkcontact/Phone.java
+++ src/com/android/sharkcontact/Phone.java
@@ -1,18 +1,47 @@
 package com.android.sharkcontact;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.provider.ContactsContract;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class Phone extends Activity {
+    private ListView contactList;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.phone);
 
-        TextView textview = new TextView(this);
-        textview.setText("拨打电话和联系人界面");
+        contactList = (ListView) findViewById(R.id.contactList);
 
-        setContentView(textview);
+        populateContactList();
+    }
+
+    private void populateContactList() {
+        Cursor cursor = getContacts();
+        String[] fields = new String[] {
+            ContactsContract.Data.DISPLAY_NAME
+        };
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.contact_entry, cursor, 
+                fields, new int[] {R.id.contactEntryText});
+        contactList.setAdapter(adapter);
+    }
+    private Cursor getContacts() {
+        // Run query
+        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+        String[] projection = new String[] {
+                ContactsContract.Contacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME
+        };
+        String selection = ContactsContract.Contacts.IN_VISIBLE_GROUP + " = '" + "1" + "'";
+        String[] selectionArgs = null;
+        String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
+
+        return managedQuery(uri, projection, selection, selectionArgs, sortOrder);
     }
 }
